dd08c1bd8e9606823fc9dbf8b3de5027ebd0a7f8
rahulchuaha <rahulchauhan572000@gmail.com>
2023-10-14 19:19:14 +0530

    solved by for loop

--- reverseArraylist.java
+++ reverseArraylist.java
@@ -1,27 +1,26 @@
  
-
-import java.io.*;
 import java.util.*;
 
-public class ArrayListPrinting {
+public class reverseArraylist {
 
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-        
+       Scanner sc = new Scanner(System.in);
         ArrayList<Integer> arr = new ArrayList<>();
-        
         int n = sc.nextInt();
+        
         for(int i=0; i<n; i++){
             int val = sc.nextInt();
-            arr.add(val);
+             arr.add(val);
         }
-        for(int i=0; i<arr.size(); i++){
+        for(int i=arr.size()-1; i>=0; i--){
             System.out.print(arr.get(i)+" ");
         }
         System.out.println();
         
+         Collections.reverse(arr);
         for(int val : arr){
             System.out.print(val+" ");
         }
     }
 }
+
